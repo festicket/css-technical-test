92fe8802e8c2ce097c042d15a40c4b3f09e0d333
aethel <aethelbrihte@gmail.com>
2017-05-25 19:37:35 +0100

    fest test

--- public/main.js
+++ public/main.js
@@ -1,1 +1,20 @@
-console.log('Add some interactivity here if you have time');
+(function() {
+    'use strict';
+
+    var repos = {
+        init: function() {
+            humanDate();
+        }
+    }
+
+    repos.init();
+
+})();
+
+function humanDate() {
+    let elements = [...document.querySelectorAll('.js-date')];
+    for (let element of elements) {
+        let date = element.getAttribute('datetime');
+        element.textContent = new Date(date).toDateString();
+    }
+}
